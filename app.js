8374cd14f7dede7b199de0c8c08011c6eb76ed57
Yao Jiang <yaj889@mail.usask.ca>
2019-05-30 11:24:15 -0600

    Roll and Hold
    rolling to 1 changes player, hold the number

--- app.js
+++ app.js
@@ -40,10 +40,38 @@ document.querySelector('.btn-roll').addEventListener('click', function() {
         document.querySelector('#current-' + activePlayer).textContent = roundScore;
     } else {
         //next player
-        activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
-        roundScore = 0;
+        nextPlayer();
     }
 });
+
+// hold
+document.querySelector('.btn-hold').addEventListener('click', function() {
+    //add current score to global score
+    scores[activePlayer] += roundScore;
+
+    //update the ui
+    document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
+
+    //check if player won the Game
+    nextPlayer();
+
+});
+
+function nextPlayer() {
+
+  //next player
+  activePlayer === 0 ? activePlayer = 1 : activePlayer = 0;
+  roundScore = 0;
+
+  document.getElementById('current-0').textContent = '0';
+  document.getElementById('current-1').textContent = '0';
+
+  document.querySelector('.player-0-panel').classList.toggle('active');
+  document.querySelector('.player-1-panel').classList.toggle('active');
+
+  document.querySelector('.dice').style.display = 'none';
+
+}
 
 //var x = document.querySelector('#score-0').textContent;
 //document.querySelector('#current-' + activePlayer).textContent = dice;
